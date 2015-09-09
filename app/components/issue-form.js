8c52e114d0a41c226726231956905fc0c3d90d29
Steven Wanderski <steven@popularpays.com>
2015-09-09 14:12:50 -0500

    Adds issue form loading state

--- app/components/issue-form.js
+++ app/components/issue-form.js
@@ -2,14 +2,19 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   issue: null,
+  isSaving: false,
 
   actions: {
     saveIssue (issue) {
+      this.set('isSaving', true);
       issue.save().then(() => {
         this.sendAction('successfulSave', this.get('issue'));
       }, function(errors){
         console.error(errors);
-      });
+      })
+      .finally(() => {
+        this.set('isSaving', false);
+      })
     },
 
     cancel () {
